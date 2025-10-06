eb3f3d1be5480be3e6942633f2ab9197fa0377c4
bhrugen <bhrugen1990@gmail.com>
2025-10-06 14:20:01 -0500

    Section 8 - API Request Model

---
+++ RoyalVillaWeb/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace RoyalVillaWeb
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
