19155d5252a29debd8d3acc99971368ea570719d
bhrugen <bhrugen1990@gmail.com>
2025-10-06 15:19:05 -0500

    Section 8 - Implement Base Service

--- RoyalVillaWeb/Services/BaseService.cs
+++ RoyalVillaWeb/Services/BaseService.cs
@@ -1,23 +1,64 @@
 ﻿using RoyalVilla.DTO;
 using RoyalVillaWeb.Models;
 using RoyalVillaWeb.Services.IServices;
+using System.Text.Json;
 
 namespace RoyalVillaWeb.Services
 {
     public class BaseService : IBaseService
     {
-        public IHttpClientFactory httpClient { get; set; }
+        public IHttpClientFactory _httpClient { get; set; }
+        private static readonly JsonSerializerOptions JsonOptions = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
         public ApiResponse<object> ResponseModel { get; set; }
 
         public BaseService(IHttpClientFactory httpClient)
         {
             this.ResponseModel = new();
-            this.httpClient = httpClient;
+            _httpClient = httpClient;
         }
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        public async Task<T?> SendAsync<T>(ApiRequest apiRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var client = _httpClient.CreateClient("RoyalVillaAPI");
+                var message = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(apiRequest.Url),
+                    Method = GetHttpMethod(apiRequest.ApiType),
+                };
+
+                if(apiRequest.Data!=null)
+                {
+                    message.Content = JsonContent.Create(apiRequest.Data,options: JsonOptions);
+                }
+
+                var apiResponse = await client.SendAsync(message);
+
+                return await apiResponse.Content.ReadFromJsonAsync<T>(JsonOptions);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected Error: {ex.Message}");
+                return default;
+            }
         }
+
+        private static HttpMethod GetHttpMethod(SD.ApiType apiType) 
+        {
+            return apiType switch
+            {
+                SD.ApiType.POST => HttpMethod.Post,
+                SD.ApiType.PUT => HttpMethod.Put,
+                SD.ApiType.DELETE => HttpMethod.Delete,
+                _ => HttpMethod.Get
+            };
+        }
+
     }
 }
