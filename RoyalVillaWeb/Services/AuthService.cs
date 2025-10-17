a055037d42cac1802557e802d23dabe502901743
bhrugen <bhrugen1990@gmail.com>
2025-10-17 14:18:50 -0500

    Section 10 - Auth Service Interface

---
+++ RoyalVillaWeb/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+
+        private const string APIEndpoint = "/api/auth";
+        public AuthService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+        }
+
+        public Task<T?> LoginAsync<T>(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> RegisterAsync<T>(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
