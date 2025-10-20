ef29b3780b2aca41a237332057c9a5a2538de7dd
bhrugen <bhrugen1990@gmail.com>
2025-10-19 20:49:09 -0500

    Add name migration

--- RoyalVilla_API/Migrations/20251020014848_addName.Designer.cs
+++ RoyalVilla_API/Migrations/20251020014848_addName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoyalVilla_API.Data;
 
@@ -11,9 +12,11 @@ using RoyalVilla_API.Data;
 namespace RoyalVilla_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251020014848_addName")]
+    partial class addName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RoyalVilla_API/Models/ApplicationUser.cs
+++ RoyalVilla_API/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace RoyalVilla_API.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        public string Name { get; set; } 
     }
 }
