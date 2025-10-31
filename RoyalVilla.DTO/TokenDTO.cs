a0ab73536386c4d33a3f0d45980981ff6c8606a2
bhrugen <bhrugen1990@gmail.com>
2025-10-31 09:23:03 -0500

    Section 16 - Add Refresh Token to Token DTO

--- RoyalVilla.DTO/TokenDTO.cs
+++ RoyalVilla.DTO/TokenDTO.cs
@@ -3,6 +3,8 @@
     public class TokenDTO
     {
         public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? ExpiresAt { get; set; }
 
     }
 }
