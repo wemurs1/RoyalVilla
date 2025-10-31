2832f21a22ff820f3a7224db2a7de02bbd30f6a6
bhrugen <bhrugen1990@gmail.com>
2025-10-31 08:25:43 -0500

    Section 16 - Small Bug Fix

--- RoyalVilla_API/Controllers/v2/VillaController.cs
+++ RoyalVilla_API/Controllers/v2/VillaController.cs
@@ -32,7 +32,8 @@ namespace RoyalVilla_API.Controllers.v2
 
 
         [HttpGet]
-        [Authorize(Roles ="Admin")]
+        //[Authorize(Roles ="Admin")]
+        [AllowAnonymous]
         [ProducesResponseType(typeof(ApiResponse<IEnumerable<VillaDTO>>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<ApiResponse<IEnumerable<VillaDTO>>>> GetVillas([FromQuery] string? filterBy,
